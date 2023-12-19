be79c192cd69521333746e27510d6891a17d1ca0
spuxx1701 <hi@spuxx.dev>
2023-12-19 19:55:32 +0100

    chore: added staging build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/).
 
-# [1.0.0] - 2023-12-19
+## [1.0.1] - unreleased
+
+### Added
+
+- Provided a `staging` build at https://auth-test.potber.de.
+
+## [1.0.0] - 2023-12-19
 
 Initial release.
--- src/lib/config/app.config.ts
+++ src/lib/config/app.config.ts
@@ -1,7 +1,9 @@
+import { env } from '$env/dynamic/private';
+
 export const appConfig = {
-	apiUrl: import.meta.env.VITE_API_URL,
-	apiLoginEndpoint: import.meta.env.VITE_API_LOGIN_ENDPOINT,
-	apiSessionEndpoint: import.meta.env.VITE_API_SESSION_ENDPOINT,
+	apiUrl: env.API_URL ?? env.VITE_API_URL,
+	apiLoginEndpoint: env.API_LOGIN_ENDPOINT ?? env.VITE_API_LOGIN_ENDPOINT,
+	apiSessionEndpoint: env.API_SESSION_ENDPOINT ?? env.VITE_API_SESSION_ENDPOINT,
 	sessionCookieName: `potber-auth-session`,
 	sessionCookieOptions: {
 		path: '/',
