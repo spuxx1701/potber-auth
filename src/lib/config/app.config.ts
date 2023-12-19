ac86a6c3d3a3451d32c746d9285a8d269c6a1cb7
spuxx1701 <hi@spuxx.dev>
2023-12-19 20:27:24 +0100

    fix: use only env variables with VITE_ prefix

--- src/lib/config/app.config.ts
+++ src/lib/config/app.config.ts
@@ -1,9 +1,9 @@
 import { env } from '$env/dynamic/private';
 
 export const appConfig = {
-	apiUrl: env.API_URL ?? env.VITE_API_URL ?? '',
-	apiLoginEndpoint: env.API_LOGIN_ENDPOINT ?? env.VITE_API_LOGIN_ENDPOINT ?? '',
-	apiSessionEndpoint: env.API_SESSION_ENDPOINT ?? env.VITE_API_SESSION_ENDPOINT ?? '',
+	apiUrl: env.VITE_API_URL ?? '',
+	apiLoginEndpoint: env.VITE_API_LOGIN_ENDPOINT ?? '',
+	apiSessionEndpoint: env.VITE_API_SESSION_ENDPOINT ?? '',
 	sessionCookieName: `potber-auth-session`,
 	sessionCookieOptions: {
 		path: '/',
