3fc235e90e4ae2a553d74bb8d135c385c72ec3a6
spuxx1701 <accounts@spuxx.dev>
2024-04-15 10:22:46 +0200

    chore: Improved devex when debugging mobile

--- src/lib/config/clients.config.ts
+++ src/lib/config/clients.config.ts
@@ -4,6 +4,7 @@ export const clients: App.Client[] = [
 		id: '45a14ddc-e3d3-4b5b-a45a-a04946974adc',
 		allowedRedirectUris: [
 			'http://localhost:4200/auth/callback',
+			'http://schleppi.fritz.box:4200/auth/callback',
 			'https://test.potber.de/auth/callback',
 			'https://potber.de/auth/callback',
 			'https://www.potber.de/auth/callback'
